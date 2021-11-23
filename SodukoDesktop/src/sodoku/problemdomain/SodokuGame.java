8876ac0be206ecb98b6dbeade321baf130c9ba39
dabben1993 <gickis1993@live.com>
2021-11-23 12:27:49 +0100

    Initial Commit

--- SodukoDesktop/src/sodoku/problemdomain/SodokuGame.java
+++ SodukoDesktop/src/sodoku/problemdomain/SodokuGame.java
@@ -1,4 +1,22 @@
 package sodoku.problemdomain;
 
-public class SodokuGame {
+import java.io.Serializable;
+
+public class SodokuGame implements Serializable {
+    private final GameState gameState;
+    private final int [][] gridState;
+
+    public static final int GRID_BOUNDARY = 9;
+
+    public SodokuGame(GameState gameState, int [][] gridState){
+        this.gameState = gameState;
+        this.gridState = gridState;
+    }
+    public GameState getGameState(){
+        return gameState;
+    }
+
+    public int[][] getCopyOfGridState() {
+        return SodukoUtilities.copyToNewArray(gridState);
+    }
 }
