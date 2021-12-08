7b66da0d79c08c248a6d318e26e2f17fa0bac0b5
dabben1993 <71631579+dabben1993@users.noreply.github.com>
2021-12-08 12:16:49 +0100

    Initialized the problem domain classes. Coordinates.java is a class to store the location of tiles in a hashmap. Interface class fo easier front to backend communication

---
+++ SodukoDesktop/src/sodoku/problemdomain/IStorage.java
@@ -0,0 +1,8 @@
+package sodoku.problemdomain;
+
+import java.io.IOException;
+
+public interface IStorage {
+    void updateGameData(SodokuGame game) throws IOException;
+    SodokuGame getGameData() throws IOException;
+}
